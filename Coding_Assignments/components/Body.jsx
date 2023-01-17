c27b7e02c0d935c817189025fce7316e794f40aa
Nishant Gautam <47427428+NishantGautam023@users.noreply.github.com>
2023-01-17 05:29:15 -0500

    Added Search  and Shimmer Effect
    Search Functionality when clicking a button and shimmer effect from the shimmer UI

--- Coding_Assignments/components/Body.jsx
+++ Coding_Assignments/components/Body.jsx
@@ -1,6 +1,7 @@
 import { restaurantList } from "../data/restaurant";
 import RestaurantCard from "./RestaurantCard";
-import { useState } from "react";
+import { useState,useEffect } from "react";
+import Shimmer from "./Shimmer";
 
 
 
@@ -8,39 +9,62 @@ import { useState } from "react";
 
 
 
-function filterRestaurant(searchText, restaurants) {
-  return restaurants.filter((eachRestaurant) =>
-    eachRestaurant.data.name.toLowerCase().includes(searchText.toLowerCase())
+function filterData(searchText, restaurants) {
+  const filterData = restaurants.filter((restaurant) =>
+    restaurant?.data?.name?.toLowerCase()?.includes(searchText.toLowerCase())
   );
+
+  return filterData;
 }
   
 
   const Body = () => {
-    const [restaurants, setRestaurants] = useState(restaurantList);
+    const [allRestaurants, setAllRestaurants] = useState([]);
+    const [filteredRestaurants, setFilteredRestaurants] = useState([]);
     const [searchText, setSearchText] = useState("");
-        
+    
+    useEffect(() => {
+      getRestaurants();
+    }, []);
+
+
+    
+  async function getRestaurants() {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    setAllRestaurants(json?.data?.cards[2]?.data?.data?.cards);
+    setFilteredRestaurants(json?.data?.cards[2]?.data?.data?.cards);
+  }
+
+  // not render component (Early return)
+  if (!allRestaurants) return null;
     
 
-        return(
+  return allRestaurants?.length === 0 ? (
+    <Shimmer />
+  ) : (
             <>  
             
                 <div className="container">
                     <div className="search">
                     
-                        <input type="text" placeholder="What are you looking for 🔎🧐🕵"  
-                           onKeyUp={(e) => {
-                            const text = e.target.value;
-                            if (text !== "") {
-                              setRestaurants(filterRestaurant(e.target.value, restaurants));
-                            } else {
-                              setRestaurants(restaurantList);
-                            }
-                            setSearchText(text);
-                          } }
-                        />
+                    <input
+                        type="text"
+                        className="search-input"
+                        placeholder="Search"
+                        value={searchText}
+                        onChange={(e) => {
+                          setSearchText(e.target.value);
+          }}
+        />
 
                          <button className="btn" onClick={() => {
-                                 
+                                   //need to filter the data
+                      const data = filterData(searchText, allRestaurants);
+                    // update the state - restaurants
+                    setFilteredRestaurants(data);
                                 
                          }} >Search</button>      
                 
@@ -49,7 +73,7 @@ function filterRestaurant(searchText, restaurants) {
                     <div className="main">
                     <div className="container-icon">
                                    
-                    {restaurants.map((restaurant) => {
+                    {filteredRestaurants.map((restaurant) => {
           return (
             <RestaurantCard {...restaurant.data} key={restaurant.data.id} />
           );
