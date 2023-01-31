662da07b18bf6d16cb5bb9aec065bcaab322d889
Nishant Gautam <47427428+NishantGautam023@users.noreply.github.com>
2023-01-31 06:53:14 -0500

    Update App.js

--- Coding_Assignments/App.js
+++ Coding_Assignments/App.js
@@ -1,4 +1,4 @@
-import React, {lazy, Suspense} from "react";
+import React, {lazy, Suspense,useEffect,useState} from "react";
 import  ReactDOM from "react-dom/client";
 import Header from "./components/Header"
 import Body from './components/Body'
@@ -14,9 +14,24 @@ import Shimmer from "./components/Shimmer";
 const Faq = lazy(() => import("./components/Faq"));
 const About = lazy(() => import("./components/About"));
 const AppLayout = () => {
+
+    const [isDarkMode, setIsDarkMode] = useState(false);
+    useEffect(() => {
+        if (isDarkMode) {
+            document.body.classList.add("dark");
+        } else {
+            document.body.classList.remove("dark");
+        }
+
+    }, [isDarkMode]);
+    const toggleDarkMode = () => {
+        setIsDarkMode((currentMode) => !currentMode);
+    };
+
   return (
     <>
-      <Header />
+      <Header isDarkMode={isDarkMode}
+              toggleDarkMode={toggleDarkMode}  />
       <Outlet />
       <Footer />
     </>
